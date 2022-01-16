896743b6ef0221e254e330614e2717bf5d305f77
Bach Phung <quangbach19112000@gmail.com>
2022-01-16 23:31:57 +0200

    Action

---
+++ client/src/authContext/Action.js
@@ -0,0 +1,9 @@
+export const loginStart = ()=>{
+    type:'LOGIN_START'
+}
+export const loginSuccess = ()=>{
+    type:'LOGIN_SUCCESS'
+}
+export const loginFailure = () =>{
+    type:'LOGIN_FAILURE'
+}
