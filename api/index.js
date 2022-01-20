a1fff5c5dbc9f9c580c1b4a8d12741e3674cfe7c
Bach Phung <quangbach19112000@gmail.com>
2022-01-20 07:48:19 +0200

    Start working with google login

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const cors = require('cors')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
@@ -12,7 +14,18 @@ mongoose.connect(process.env.MONGO_URL, {
 })
     .then(() => console.log('Connected to mongoose'))
     .catch((err) => console.log(err))
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials:true,
+}))
+app.use(cookieSession({
+    name:'session',
+    keys:["back"],
+    maxAge: 24 * 60 * 60 * 100
+}))
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
