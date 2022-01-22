d2733b41ce7c3d5e7d88945bae6167c9b1f23445
Bach Phung <quangbach19112000@gmail.com>
2022-01-22 23:47:29 +0200

    Passport

--- api/passport.js
+++ api/passport.js
@@ -1,13 +1,15 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('./models/User')
+const passport = require('passport')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://www.example.com/auth/google/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    done(null,profile)
   }
 ));
